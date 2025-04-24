c063c981893b85611e736e694e15f5780ec13382
Muneeb-Babar <muneeb63ahmad63@gmail.com>
2025-04-24 13:02:10 +0500

    update

--- src/validations/authValidation.js
+++ src/validations/authValidation.js
@@ -5,11 +5,11 @@ export const registerValidation = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string()
     .min(8)
-    .pattern(
-      new RegExp(
-        "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$"
-      )
-    )
+    // .pattern(
+    //   new RegExp(
+    //     "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$"
+    //   )
+    // )
     .required()
     .messages({
       "string.pattern.base":
